b96aa64cecf462aba89ae40bf887ea139ab0aff8
lukasmetzlaf <lukasmetzlaf@172.20.0.199>
2016-03-23 13:34:49 +0100

    j

---
+++ monopoly/src/valtech/germanTeam/bo/TokenShape.java
@@ -0,0 +1,8 @@
+package valtech.germanTeam.bo;
+
+/*
+ * TokenShape  is keeping information about the usable tokens
+ */
+public enum TokenShape {
+	DOG, IRON, SHIP, CANNON, TOP_HAT, HORSE, SHOE, CAR
+}
