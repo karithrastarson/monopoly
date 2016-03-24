823584714b21653d7126e87fe1048c996fbea47d
lukasmetzlaf <lukasmetzlaf@172.20.0.199>
2016-03-24 17:00:23 +0100

    added test for the die

---
+++ monopoly_tests/src/valtech/germanTeam/bo/TestDie.java
@@ -0,0 +1,15 @@
+package valtech.germanTeam.bo;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestDie {
+
+	@Test
+	public void testDie(){
+		Die die = new Die();
+	for(int i = 0;i<10; i++){
+		assertTrue(die.generateValue()<7 && die.generateValue()>0);
+	}
+	}
+
+}
