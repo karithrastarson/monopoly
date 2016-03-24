e8d18a62e7767eea3fa1301e1527ec04d95dae88
lukasmetzlaf <lukasmetzlaf@172.20.0.199>
2016-03-24 16:00:52 +0100

    changed the creator, the game and its interface

--- monopoly/src/valtech/germanTeam/service/IGame.java
+++ monopoly/src/valtech/germanTeam/service/IGame.java
@@ -1,14 +1,9 @@
-package valtech.germanTeam.service;
+ package valtech.germanTeam.service;
 
-import valtech.germanTeam.bo.Player;
+
 
 public interface IGame {
 
 	void startGame();
-	void initBoard();
-	void initDie();
-	void addPlayer(Player p, int position);
-	void removePlayer(int position);
-	void doRound();
-
+	void initGame(short numberOfPlayers);
 }
