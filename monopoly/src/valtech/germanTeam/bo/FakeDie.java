ba7de94d0296bf7c470f092189afb3da554faf94
lukasmetzlaf <lukasmetzlaf@172.20.0.199>
2016-03-24 16:38:36 +0100

    added a test for the movement of the token

---
+++ monopoly/src/valtech/germanTeam/bo/FakeDie.java
@@ -0,0 +1,13 @@
+package valtech.germanTeam.bo;
+
+public class FakeDie extends Die {
+
+	public FakeDie() {
+		super();
+	}
+	
+	public int generateValue(){ 
+		return (1);
+	}
+
+}
--- monopoly_tests/src/valtech/germanTeam/bo/TestPlayer.java
+++ monopoly_tests/src/valtech/germanTeam/bo/TestPlayer.java
@@ -5,12 +5,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import valtech.germanTeam.service.Game;
+
 public class TestPlayer {
 	
+	/*
+	 * checks if the Player moves the token in the defined way
+	 */
+	
 	@Test
-	public void TestInitialAmount()
+	public void TestPlayerMovement() throws Exception
 	{
-			//TODO
+		Game game = Game.getInstance();
+		game.initGame((short)2);
+		
+		Player p = game.getPlayers()[0];
+		FakeDie fd1 = new FakeDie();
+		FakeDie fd2 = new FakeDie();
+		Space startPosition = p.getToken().getCurrentSpace();
+		int throwSum = p.throwDice(fd1, fd1);
+		p.moveToken(throwSum);
+		
+		String endPosition = p.getToken().getCurrentSpace().spaceName;
+		
+		/*
+		 * get the reference space for the test that represents the field the 
+		 * token is supposed to move to
+		 * for the fake dice show allways 1, the token should move 2 spaces forward
+		 */
+		Space referenceEndSpace = startPosition.nextSpace.nextSpace;
+		String referenceEndSpaceName= referenceEndSpace.spaceName;
+		
+		
+		assertTrue(referenceEndSpaceName.equals(endPosition));
 	}
 
 }
